dac50f027bc8951e7c97a7d377c17e35ecedf3ff
olemathgeek <e.kristense@gmail.com>
2018-03-05 21:33:58 -0600

    fix bugs and simplify code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,109 +15,52 @@ export class AppComponent {
 	npr = [];
 	disney = [];
 
-	counter0 = 0;
-	timer0Id: string;
-	timer0button = 'Subscribe';
-
-	counter1 = 0;
-	timer1Id: string;
-	timer1button = 'Subscribe';
-
-	counter2 = 0;
-	timer2Id: string;
-	timer2button = 'Subscribe';
-
 	constructor(private st: SimpleTimer, private rssService: RssService) { }
 
 	ngOnInit() {
 		this.st.newTimer('1sec',1);
 		this.st.newTimer('300sec',300);
 		this.st.newTimer('60sec',60);
-		this.subscribeTimer0();
 		this.subscribeTimer1();
-		this.subscribeTimer2();
-	}
-
-	delAllTimer() {
-		this.st.delTimer('1sec');
-		this.st.delTimer('5sec');
-		this.st.delTimer('10sec');
-	}
-
-	subscribeTimer0() {
-		if (this.timer0Id) {
-			// Unsubscribe if timer Id is defined
-			this.st.unsubscribe(this.timer0Id);
-			this.timer0Id = undefined;
-			this.timer0button = 'Subscribe';
-			console.log('timer 0 Unsubscribed.');
-		} else {
-			// Subscribe if timer Id is undefined
-			this.timer0Id = this.st.subscribe('1sec', () => this.timer0callback());
-			this.timer0button = 'Unsubscribe';
-			console.log('timer 0 Subscribed.');
-		}
-		console.log(this.st.getSubscription());
+		this.subscribeTimer300();
+		this.subscribeTimer60();
 	}
 
 	subscribeTimer1() {
-		if (this.timer1Id) {
-			// Unsubscribe if timer Id is defined
-			this.st.unsubscribe(this.timer1Id);
-			this.timer1Id = undefined;
-			this.timer1button = 'Subscribe';
-			console.log('timer 1 Unsubscribed.');
-		} else {
-			// Subscribe if timer Id is undefined
-			this.timer1Id = this.st.subscribe('5sec', () => this.timer1callback());
-			this.timer1button = 'Unsubscribe';
-			console.log('timer 1 Subscribed.');
-		}
-		console.log(this.st.getSubscription());
+		this.st.subscribe('1sec', () => this.timer1callback());
 	}
 
-	subscribeTimer2() {
-		if (this.timer2Id) {
-			// Unsubscribe if timer Id is defined
-			this.st.unsubscribe(this.timer2Id);
-			this.timer2Id = undefined;
-			this.timer2button = 'Subscribe';
-			console.log('timer 2 Unsubscribed.');
-		} else {
-			// Subscribe if timer Id is undefined
-			this.timer2Id = this.st.subscribe('10sec', () => this.timer2callback());
-			this.timer2button = 'Unsubscribe';
-			console.log('timer 2 Subscribed.');
-		}
-		console.log(this.st.getSubscription());
+	subscribeTimer300() {
+		this.st.subscribe('300sec', () => this.timer300callback());
 	}
 
-	timer0callback() {
-	//every 1 seconds
-	  this.printDate = new Date();
-		this.counter0++;
+	subscribeTimer60() {
+		this.st.subscribe('60sec', () => this.timer60callback());
 	}
 
 	timer1callback() {
-	//every 5 minutes
-	//http://api.wunderground.com/api/ebe965a96fa2346b/conditions/q/MN/Osseo.json
-		this.counter1++;
+	// //every 1 seconds
+	  this.printDate = new Date();
 	}
 
-	timer2callback() {
+	timer300callback() {
+	// every 5 minutes
+	// http://api.wunderground.com/api/ebe965a96fa2346b/conditions/q/MN/Osseo.json
+	}
+
+	timer60callback() {
 	//every 1 minutes
 		this.printDate = new Date()//.format('D, M d, Y');
 	 	this.rssService.getNprRss().toPromise().then(data => {
-        	this.npr = data.items;
+			this.npr = data.items;
+			console.log('npr:');
+			console.log(data);
   		});
 	 	this.rssService.getDisneyRss().toPromise().then(data => {
         	this.disney = data.items;
-        	console.log(data.items);
+			console.log('disney:');
+			console.log(data);
   		});
-	 
-
-		this.counter2++;
 	}
-
 
 }
